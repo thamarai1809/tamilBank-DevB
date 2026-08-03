3fcad4f468cf41ab25e5cc6ab8360572168d307d
thamarai1809 <thamarairajkumar26@gmail.com>
2026-08-03 06:23:59 +0000

    Clean up gitignore, untrack venv and remove leftover cv-dataset clone

--- venv/Scripts/uigtransliterate.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\thama\tamilbank-devB\venv\Scripts\python.exe
-
-import fileinput
-import epitran
-
-epi = epitran.Epitran('uig-Arab')
-for line in fileinput.input():
-    s = epi.transliterate(line.strip())
-    print(s)
